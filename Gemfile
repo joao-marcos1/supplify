publify/publify

commit 4deb03f7f698dfe6a308dc301515a2dffc16bca1

flickraw require changed

Gemfile
@@ -33,7 +33,7 @@ gem 'RedCloth', '~> 4.2.8'
 gem 'addressable', '~> 2.1', :require => 'addressable/uri'
 gem 'mini_magick', '~> 1.3.3', :require => 'mini_magick'
 gem 'uuidtools', '~> 2.1.1'
-gem 'flickraw-cached'
+gem 'flickraw-cached', :require => 'flickraw'
 gem 'rubypants', '~> 0.2.0'
 gem 'rake', '~> 0.9.2'
 gem 'acts_as_list'

joao-marcos1/supplify

commit 73f6b1813ece51bd81da4659f74093fe0c760642

Really use flickraw-cached.
The way the Gemfile was changed after
4deb03f7f698dfe6a308dc301515a2dffc16bca1 caused flickraw (non-cached)
to be loaded instead. This is one of the two way to straighten this,
the alternative is to just require flickraw. Either seem to work.

Gemfile
@@ -32,7 +32,7 @@ gem 'RedCloth', '~> 4.2.8'
 gem 'addressable', '~> 2.1', :require => 'addressable/uri'
 gem 'mini_magick', '~> 1.3.3', :require => 'mini_magick'
 gem 'uuidtools', '~> 2.1.1'
-gem 'flickraw-cached', :require => 'flickraw'
+gem 'flickraw-cached'
 gem 'rubypants', '~> 0.2.0'
 gem 'rake', '~> 10.0.2'
 gem 'acts_as_list'